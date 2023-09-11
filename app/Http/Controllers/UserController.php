602f44da49dfaced92a81228f6a8531bedf5008c
pavel <213633@mail.ru>
2023-09-11 23:05:16 +0300

    download

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
+/**
+ * @OA\Info(
+ *     title="My doc api",
+ *     version="1.0.0"
+ * ),
+ * @OA\PathItem(
+ *     path="/api/"
+ * )
+.*/
 class UserController extends Controller
 {
-
+    /**
+    @OA\Get(
+     *        path="/api/hello/{name}",
+     *        summary="Get hello name",
+     *        tags={"name"},
+     *        @OA\Parameter(
+     *        description="Get hello name",
+     *        in="path",
+     *        name="name",
+     *        required=true,
+     *
+     *        ),
+     *
+     *         @OA\Response(
+     *            response=200,
+     *            description="ok",
+     *              @OA\JsonContent(
+     *                              @OA\Property(property="message", type="string", example="hello Pavel"),
+     *
+     *              ),
+     *        ),
+     *  ),
+     *
+     */
 
     public function show($name){
 
